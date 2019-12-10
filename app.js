977114d462b046a5dde81c8fab9fb269bbc8674e
Eliecer Gerdler <eliecergerdler@gmail.com>
2019-12-10 16:30:00 -0400

    se practico operaciones en javascript

--- app.js
+++ app.js
@@ -28,7 +28,52 @@ let aprendiendo = 'Aprendiendo',
 console.log(`${aprendiendo} ${tecnologia}`);
 */
 
+/*
 let aprendiendo = 'Aprendiendo',
     tecnologia = 'Javascript';
 
     console.log(tecnologia.toUpperCase());
+*/
+
+/*Numeros en JavaScript
+
+const numero1 = 30;
+      numero2 = 20;
+      numero3 = 20.20;
+      numero4 = .1020;
+      numero5 = -3;
+
+let resultado;
+
+//suma
+resultado = numero1 + numero2;
+// resta
+resultado = numero1 + numero2;
+// multiplicar
+resultado = numero1 * 2;
+// divisiones
+resultado = numero1 / numero2;
+// modulo = es el residuo de una division
+resultado = numero1 % numero2;
+// Pi
+resultado = Math.PI;
+// Redondeo hacia arriba ceil y para abajo floor
+resultado = Math.round(2.5);
+// Raiz cuadrada
+resultado = Math.sqrt(144);
+// absoluto
+resultado = Math.abs(numero5);
+//potencia
+resultado = Math.pow(8, 3);
+// minimo
+resultado = Math.min(3,6,5,9,7,8);
+// maximo 
+resultado = Math.max(3,6,5,9,7,8);
+// Aleatorio
+resultado = Math.random();
+
+
+
+
+console.log(resultado);*/
+
