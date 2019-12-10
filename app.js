282af49a7c5d2753fbe054600e98a806b91e9df4
Eliecer Gerdler <eliecergerdler@gmail.com>
2019-12-10 16:07:48 -0400

    se practico funciones y variables con sus metodos

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-document.getElementById('app').innerHTML = 'Hola Mundo';
+/*var nombre = 'Juan';
+nombre = 'Jhon';
+
+console.log(nombre);
+*/
+
+
+//string
+
+let nombre, mensaje;
+
+nombre = 'Juan Pablo';
+nombre = "Juan Pablo";
+
+mensaje = "y entonces dije 'Buen curso' ";
+
+mensaje = 20;
+
+/*concatenar
+
+mensaje = 'JAVA' + 'SCRIPT';
+
+//Uniendo variables en una sola
+
+let aprendiendo = 'Aprendiendo',
+    tecnologia = 'Javascript';
+
+console.log(`${aprendiendo} ${tecnologia}`);
+*/
+
+let aprendiendo = 'Aprendiendo',
+    tecnologia = 'Javascript';
+
+    console.log(tecnologia.toUpperCase());
